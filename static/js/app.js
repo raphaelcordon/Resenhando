24b822dda4ed752dc7bca3757a2fdd70334562b4
Raphael Cordon <cordon@outlook.com>
2020-06-26 16:06:10 +0200

    Fixing auto update of images

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,13 @@
-$('form input[type="file"]').change(event => {
-  let arquivos = event.target.files;
-  if (arquivos.length === 0) {
-    console.log('sem imagem pra mostrar')
-  } else {
-      if(arquivos[0].type == 'image/png') {
-        $('img').remove();
-        let imagem = $('<img class="img-responsive">');
-        imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
-      elif(arquivos[0].type == 'image/jpeg') {
-        $('img').remove();
-        let imagem = $('<img class="img-responsive">');
-        imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
-        $('figure').prepend(imagem);
-      elif(arquivos[0].type == 'image/jpg') {
-        $('img').remove();
-        let imagem = $('<img class="img-responsive">');
-        imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
-      } else {
-        alert('Formato não suportado')
-      }
-  }
-});
+function handleFileSelect(evt) {
+        var files = evt.target.files;
+        var f = files[0];
+        var reader = new FileReader();
+         
+          reader.onload = (function(theFile) {
+                return function(e) {
+                  document.getElementById('list').innerHTML = ['<img src="', e.target.result,'" title="', theFile.name, '" width="500" />'].join('');
+                };
+          })(f);
+           
+          reader.readAsDataURL(f);
+}
