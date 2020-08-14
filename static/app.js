34b7cd2074457deb1e15bc4aca0e335064fd1d67
Raphael Cordon <cordon@outlook.com>
2020-08-14 16:03:18 +0200

    Sorting resenhaIndex

--- static/app.js
+++ static/app.js
@@ -1,28 +1,3 @@
-$('form input[type="file"]').change(event => {
-  let arquivos = event.target.files;
-  if (arquivos.length === 0) {
-    console.log('sem imagem pra mostrar')
-  } else {
-    if (arquivos[0].type == 'image/png' || arquivos[0].type == 'image/jpeg') {
-      $('img').remove();
-      let imagem = $('<img class="img-responsive">');
-      imagem.attr('src', window.URL.createObjectURL(arquivos[0]));
-      $('figure').prepend(imagem);
-    } else {
-      alert('Formato não suportado')
-    }
-  }
-});
-
-function validateForm() {
-  var spotify = $("#spotify_link");
-  if (spotify.val().index0f('spotify.com') < 0) {
-    alert("Link do Spotify e invalido");
-    spotify.focus();
-    return false;
-  }
-}
-
 $('.delete').click(function () {
   var url = this.dataset.url;
   bootbox.confirm({
@@ -34,4 +9,48 @@ $('.delete').click(function () {
       }
     }
   })
+});
+
+// Artist
+$(document).ready(function(){
+  $("#buttonArtist").click(function(){
+
+    $("#formAlbum").hide(1000);
+    $("#formTrack").hide(1000);
+    $("#formPlaylist").hide(1000);
+    $("#formArtist").toggle(1000);
+  });
+});
+
+// Album
+$(document).ready(function(){
+  $("#buttonAlbum").click(function(){
+
+      $("#formArtist").hide(1000);
+      $("#formTrack").hide(1000);
+      $("#formPlaylist").hide(1000);
+    $("#formAlbum").toggle(1000);
+  });
+});
+
+// Track
+$(document).ready(function(){
+  $("#buttonTrack").click(function(){
+
+    $("#formArtist").hide(1000);
+    $("#formAlbum").hide(1000);
+    $("#formPlaylist").hide(1000);
+    $("#formTrack").toggle(1000);
+  });
+});
+
+// Playlist
+$(document).ready(function(){
+  $("#buttonPlaylist").click(function(){
+
+    $("#formArtist").hide(1000);
+    $("#formAlbum").hide(1000);
+    $("#formTrack").hide(1000);
+    $("#formPlaylist").toggle(1000);
+  });
 });
