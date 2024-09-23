b6bbc9c3630bf1db17d4fbb54114d561095c13af
Raphael Cordon <56551789+raphaelcordon@users.noreply.github.com>
2024-09-23 13:13:48 +0200

    Update base_repos.py

--- repository/base_repos.py
+++ repository/base_repos.py
@@ -4,11 +4,11 @@ import psycopg2
 class PostgreDB:
     def __init__(self):
         self.conn = psycopg2.connect(
-            host='ec2-52-31-70-136.eu-west-1.compute.amazonaws.com',
-            database='d37k7t1n9j0gnt',
-            user='jsjtbbbgrlwylu',
+            host='cdju6b3s1cdqa9.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com',
+            database='d16lga7geo7bjv',
+            user='u96inmogjs8uii',
             port='5432',
-            password='fd7046128d574538515e2fb9a30347c56cbd6e50ada6a57f59b9c7dcd90b605f'
+            password='p65de7c2f3f380da6d6bdbe9cb96b5311e8fdfbc1478f521ab10077cafc15d6f2'
         )
         self.cur = self.conn.cursor()
 
